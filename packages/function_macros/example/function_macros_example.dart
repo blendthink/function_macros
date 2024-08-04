2973efb1a6f78bb6c2d974b5d092166a54b5b5be
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-08-04 12:25:37 +0900

    feat: サンプルを作成

--- packages/function_macros/example/function_macros_example.dart
+++ packages/function_macros/example/function_macros_example.dart
@@ -1,7 +1,75 @@
+// ignore_for_file: avoid_print
+
 import 'package:function_macros/function_macros.dart';
 
+@CallbackOrNull()
+external String sampleA(
+  String a, [
+  String? b,
+]);
+
+@CallbackOrNull()
+external String sampleB(
+  String a, {
+  required String? b,
+});
+
+@CallbackOrNull()
+external String _sampleC(
+  String a,
+);
+
 void main() {
-  final awesome = Awesome();
-  // ignore: avoid_print
-  print('awesome: ${awesome.isAwesome}');
+  print('Start sampleA');
+  demoSampleA();
+
+  print('Start sampleB');
+  demoSampleB();
+
+  print('Start sampleC');
+  demoSampleC();
+}
+
+void demoSampleA() {
+  SampleAFunction? sample;
+  final result1 = sample.callbackOrNull('a', 'b');
+  print(result1);
+
+  sample = (
+    String a, [
+    String? b,
+  ]) {
+    return a + (b ?? '');
+  };
+  final result2 = sample.callbackOrNull('a', 'b');
+  print(result2);
+}
+
+void demoSampleB() {
+  SampleBFunction? sample;
+  final result1 = sample.callbackOrNull('a', b: 'b');
+  print(result1);
+
+  sample = (
+    String a, {
+    required String? b,
+  }) {
+    return a + (b ?? '');
+  };
+  final result2 = sample.callbackOrNull('a', b: 'b');
+  print(result2);
+}
+
+void demoSampleC() {
+  _SampleCFunction? sample;
+  final result1 = sample.callbackOrNull('a');
+  print(result1);
+
+  sample = (
+    String a,
+  ) {
+    return a;
+  };
+  final result2 = sample.callbackOrNull('a');
+  print(result2);
 }
