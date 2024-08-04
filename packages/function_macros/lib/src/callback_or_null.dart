5fb03743e34d315e6f2d2ef4ca80a7f0d0276417
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-08-04 12:18:46 +0900

    feat: CallbackOrNul macro を実装

--- packages/function_macros/lib/src/callback_or_null.dart
+++ packages/function_macros/lib/src/callback_or_null.dart
@@ -19,5 +19,131 @@ macro class CallbackOrNull implements FunctionTypesMacro {
       );
       throw DiagnosticException(diagnostic);
     }
+
+    final originalFunctionName = function.identifier.name;
+    final functionName = originalFunctionName.toCamelCase + 'Function';
+    final callBackName = originalFunctionName.toCamelCase + 'Callback';
+    final extensionName = callBackName + 'OrNull';
+
+    final returnTypeCode = function.returnType.code;
+
+    builder.declareType(
+      functionName,
+      DeclarationCode.fromParts([
+        'typedef $functionName = ',
+        returnTypeCode,
+        ' Function',
+        function.buildArgumentsCode,
+        ';',
+      ]),
+    );
+
+    builder.declareType(
+      callBackName,
+      DeclarationCode.fromParts([
+        'typedef $callBackName = ',
+        returnTypeCode,
+        ' Function();',
+      ]),
+    );
+
+    builder.declareType(
+      extensionName,
+      DeclarationCode.fromParts([
+        'extension $extensionName on $functionName? {\n',
+        '  $callBackName? callbackOrNull',
+        function.buildArgumentsCode,
+'''
+{
+    final tmp = this;
+    if (tmp == null) {
+      return null;
+    }
+    return () => tmp
+''',
+        function.buildArgumentsCodeForInput,
+''';
+''',
+'''
+  }
+}
+''',
+      ]),
+    );
+  }
+}
+
+extension _CamelCase on String {
+  String get toCamelCase {
+    if (isEmpty) {
+      return '';
+    }
+    
+    final firstText = this[0];
+    if (length == 1) {
+      if (!firstText.contains(RegExp(r'[a-zA-Z]'))) {
+        throw FormatException('Invalid character: $this');
+      }
+      return toUpperCase();
+    }
+    
+    if (firstText == '_') {
+      final secondText = this[1];
+      return '_' + secondText.toUpperCase() + substring(2);
+    }
+
+    return firstText.toUpperCase() + substring(1);
+  }
+}
+
+extension _BuildArgumentsCode on FunctionDeclaration {
+  DeclarationCode get buildArgumentsCode {
+    return DeclarationCode.fromParts([
+      '(',
+      if (positionalParameters.any((p) => p.isRequired)) ...[  
+        for (final positionalRequired in positionalParameters
+            .where((p) => p.isRequired)) ...[
+          positionalRequired.code,
+          ', ',
+        ],
+      ],
+      if (positionalParameters.any((p) => !p.isRequired)) ...[
+        '[',
+        for (final positionalOptional in positionalParameters
+            .where((p) => !p.isRequired)) ...[
+          positionalOptional.code,
+          ', ',
+        ],
+        ']',
+      ],
+      if (namedParameters.isNotEmpty) ...[
+        '{',
+        for (final named in namedParameters) ...[
+          named.code,
+          ', ',
+        ],
+        '}',
+      ],
+      ')',
+    ]);
+  }
+
+  DeclarationCode get buildArgumentsCodeForInput {
+    return DeclarationCode.fromParts([
+      '(',
+      if (positionalParameters.isNotEmpty) ...[  
+        for (final positionalParameter in positionalParameters) ...[
+          positionalParameter.name,
+          ', ',
+        ],
+      ],
+      if (namedParameters.isNotEmpty) ...[
+        for (final named in namedParameters) ...[
+          '${named.name}: ${named.name}',
+          ', ',
+        ],
+      ],
+      ')',
+    ]);
   }
 }
